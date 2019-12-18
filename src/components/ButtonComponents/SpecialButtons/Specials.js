33cd7195883aee246bed40a2577ce85d30969221
yusufnafey <yusufnafey@yahoo.com>
2019-12-18 13:41:28 -0800

    all data being passed correctly

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { specials } from '../../../data'
+import SpecialButton from './SpecialButton'
 
 //import any components needed
 
@@ -6,12 +8,15 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialValue, setSpecialValue] = useState(specials)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specialValue.map(special => <SpecialButton text={special} />)}
     </div>
   );
 };
+
+export default Specials
